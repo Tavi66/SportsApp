cb63091688562807643189e51bbe632329ebccac
Tavi66 <42556132+Tavi66@users.noreply.github.com>
2019-01-27 09:00:10 -0600

    add events
    insert events cannot read event yet

--- SportsAppX/app/src/main/java/hack/sportsapp/AddEventActivity.java
+++ SportsAppX/app/src/main/java/hack/sportsapp/AddEventActivity.java
@@ -5,25 +5,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class AddEventActivity extends AppCompatActivity {
 
-    EditText editTextEventName, eventLocation, eventDate;
+    EditText editTextEventName, eventLocation;
+    Calendar eventDate;
     SQLiteDatabase mDatabase;
 
     public void addEvent(View view)
     {
         editTextEventName = (EditText) findViewById(R.id.EventName);
-        eventDate = (EditText) findViewById(R.id.editTextDate);
-        String eventName = editTextEventName.getText().toString().trim();
-        String date = eventDate.getText().toString();
-        String insertSQL = "INSERT INTO events \n" +
-                "DEFAULT VALUES);";
+        eventLocation = (EditText) findViewById(R.id.editTextLocation);
+        String eventName = editTextEventName.getText().toString();
+        String location = eventLocation.getText().toString();
+//        Calendar cal = eventDate.getInstance();
+//        SimpleDateFormat sdf = new SimpleDateFormat("mm-dd-yyyy");
+        String date = "2019-01-10";
 
+        mDatabase = openOrCreateDatabase("usersdb", MODE_PRIVATE, null);
+        mDatabase.execSQL(
+                "CREATE TABLE IF NOT EXISTS events (\n" +
+                        " eventName varchar, date datetime, location varchar\n" + ")" +
+                        ";"
+        );
+        String insertSQL = "INSERT INTO events \n" +
+               // "(eventName, date, location)\n" +
+                //"VALUES(eventName,date,location);";
+                "VALUES\n" + "('" + eventName + "','" + date + "','" + location + "');";
+        mDatabase.execSQL(insertSQL);
+
+        //Toast.makeText(this, "Event Added Successfully!", Toast.LENGTH_SHORT);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+        //editTextEventName = "Event";
+        //eventLocation = "Event";
     }
 }
--- SportsAppX/app/src/main/java/hack/sportsapp/MainActivity.java
+++ SportsAppX/app/src/main/java/hack/sportsapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import android.database.Cursor;
 public class MainActivity extends AppCompatActivity {
 
     public static final String DATABASE_NAME = "usersdb";
@@ -19,12 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDatabase = openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
+
+//        createEventsTable();
+
+    }
+    public String[] readEvent(View view)
+    {
+        mDatabase = openOrCreateDatabase("usersdb", MODE_PRIVATE, null);
+        String selectQuery = "SELECT * FROM events";
+        SQLiteDatabase db = mDatabase;
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        String [] data = null;
+
+        if(cursor.moveToFirst()){
+
+            do{}while(cursor.moveToNext());
+        }
+        cursor.close();
+        return data;
+
     }
 
-    private void addEvent(){
-    }
-
-    private void createEventsTable(){
+    public void createEventsTable(){
         mDatabase.execSQL(
                 "CREATE TABLE IF NOT EXISTS events (\n" +
                         " eventName varchar(30) NOT NULL PRIMARY KEY, \n" +
